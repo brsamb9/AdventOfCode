4f049e5ef972ac26a4a5fe24e0e35a7a0a63374f
Ben Sambells <brsamb9@gmail.com>
2020-12-20 22:31:57 +0000

    day_17 p1

--- day_17/src/lib.rs
+++ day_17/src/lib.rs
@@ -1,99 +1,71 @@
-use std::collections::{hash_map::IterMut, HashMap, VecDeque};
-use std::iter;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+struct Coordinate(i64, i64, i64);
 
 #[derive(Debug, Clone)]
-struct Slice2D(VecDeque<VecDeque<bool>>);
+pub struct ConwayGrid(HashMap<Coordinate, bool>);
 // true == active, false == inactive
 
-impl Slice2D {
-    fn new(inital: &str) -> Self {
-        let cross_section = inital
-            .split("\n")
-            .map(|l| {
-                l.chars()
-                    .map(|l| match l {
-                        '.' => false,
-                        '#' => true,
-                        _ => panic!("Error: unrecognised input"),
-                    })
-                    .collect::<VecDeque<bool>>()
-            })
-            .collect::<VecDeque<VecDeque<bool>>>();
-
-        Slice2D(cross_section)
-    }
-
-    fn empty(size: &usize) -> Self {
-        // Square
-        let v: VecDeque<bool> = iter::repeat(false).take(*size).collect();
-        let frame: VecDeque<VecDeque<bool>> = iter::repeat(v).take(*size).collect();
-        Slice2D(frame)
-    }
-    fn number_of_alive_in_frame(&self) -> usize {
-        // Not elegent ->
-        self.0
-            .iter()
-            .map(|v| v.iter().filter(|&i| *i).count())
-            .sum()
-    }
-}
-#[derive(Debug, Clone)]
-struct ConwayGrid(HashMap<i32, Slice2D>);
-
 impl ConwayGrid {
-    pub fn new(inital: &str, zID: i32) -> Self {
-        let mut map: HashMap<i32, Slice2D> = HashMap::new();
+    pub fn new(inital: &str) -> Self {
+        let mut grid: HashMap<Coordinate, bool> = HashMap::new();
 
-        let initial: Slice2D = Slice2D::new(inital);
-        map.insert(zID, initial);
-
-        ConwayGrid(map)
+        for (row, line) in inital.split("\n").enumerate() {
+            for (col, letter) in line.chars().enumerate() {
+                let pos = Coordinate(row as i64, col as i64, 0);
+                let state = match letter {
+                    '#' => true,
+                    '.' => false,
+                    _ => unimplemented!(),
+                };
+                grid.insert(pos, state);
+            }
+        }
+        ConwayGrid(grid)
     }
 
     pub fn total_active_states(&self) -> usize {
-        self.0
-            .iter()
-            .fold(0, |acc, (_, v)| acc + v.number_of_alive_in_frame())
+        self.0.len()
     }
 
-    pub fn simulate_turn(&mut self) {
-        let state_copy = self.0.clone();
-        self.apply_rules(&state_copy);
-    }
+    pub fn simulate(&self) -> Self {
+        let mut map_neigh_count: HashMap<Coordinate, u32> = HashMap::new();
 
-    fn apply_rules(&self, state_copy: &HashMap<i32, Slice2D>) {
-        let should_expand = false;
-        if should_expand {
-            self.expansion();
+        for (k, v) in self.0.iter() {
+            if *v {
+                for x in -1..=1 {
+                    for y in -1..=1 {
+                        for z in -1..=1 {
+                            if x == 0 && y == 0 && z == 0 {
+                                continue;
+                            }
+                            let coord = Coordinate(k.0 + x, k.1 + y, k.2 + z);
+                            let new_entry = map_neigh_count.entry(coord).or_insert(0);
+                            *new_entry += 1;
+                        }
+                    }
+                }
+            }
         }
-    }
+        let mut next_state: HashMap<Coordinate, bool> = HashMap::new();
 
-    fn expansion(&mut self) {
-        // Add inactive dimensions to edges. E.g. Z = 0 -> Z = -1, 0, 1
-        let size: &usize = &self.0[&0].0[0].len();
-
-        let minD = &self.0.keys().min().unwrap();
-        self.0.insert(*minD - 1, Slice2D::empty(size));
-
-        let maxD = &self.0.keys().max().unwrap();
-        self.0.insert(*maxD + 1, Slice2D::empty(size));
-
-        // And expand the vectors -> e.g. 0 to left and right - [0,1,1,0,1] -> [0,0,1,1,0,1,0]
-        self.0.iter_mut().for_each(|(_, frame)| {
-            frame.0.iter_mut().for_each(|v| {
-                v.push_front(false);
-                v.push_back(false);
-            })
-        });
-    }
-
-    fn neighbours(&self, index: (u32, u32, i32)) -> [bool; 26] {
-        // Infinite -> no edges to worry about
-        [true; 26]
-    }
-
-    fn atIndex(&self, x: usize, y: usize, z: i32) -> bool {
-        self.0[&z].0[x][y]
+        for (k, v) in map_neigh_count {
+            match v {
+                3 => {
+                    next_state.insert(k, true);
+                }
+                2 => {
+                    if let Some(&b) = self.0.get(&k) {
+                        if b {
+                            next_state.insert(k, true);
+                        }
+                    }
+                }
+                _ => continue,
+            }
+        }
+        ConwayGrid(next_state)
     }
 }
 
@@ -102,12 +74,11 @@ mod test {
     use super::*;
     #[test]
     fn test_struct() {
-        let initial_2D_slice = ".#.\n..#\n###";
-        let mut Game = ConwayGrid::new(initial_2D_slice, 0);
-
-        assert!(5 == Game.total_active_states());
-        println!("{:?}", Game);
-        Game.simulate_turn();
-        println!("{:?}", Game);
+        let initial_2d_slice = ".#.\n..#\n###";
+        let mut game = ConwayGrid::new(initial_2d_slice);
+        for _ in 0..6 {
+            game = game.simulate();
+        }
+        assert!(112 == game.total_active_states());
     }
 }
--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,3 +1,27 @@
+use day_17::ConwayGrid;
+
 fn main() {
-    println!("Hello, world!");
+    let input: &str =
+        ".##.####\n.#.....#\n#.###.##\n#####.##\n#...##.#\n#######.\n##.#####\n.##...#.\n";
+    println!(
+        "Part 1 (3D) Active states after six turns: {}",
+        part_one(input)
+    );
+    println!(
+        "Part 2 (4D) Active states after six turns: {}",
+        part_two(input)
+    );
+}
+
+fn part_one(input: &str) -> usize {
+    let mut game = ConwayGrid::new(input);
+    println!("{:?}", game);
+    for _ in 0..6 {
+        game = game.simulate();
+    }
+    game.total_active_states()
+}
+
+fn part_two(input: &str) -> usize {
+    unimplemented!()
 }
